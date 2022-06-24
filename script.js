85edf016ea17478493b584fd9edf52166372f99a
Toyalp <97072609+Toyalp@users.noreply.github.com>
2022-06-23 20:01:08 -0400

    Updated Changes

--- script.js
+++ script.js
@@ -8,24 +8,121 @@
 //  1. Based on each day's weather, recommend an activity for the user — e.g., 
 // "A great day for golfing," or "Reading a book inside might be nice."
 
-const wind = ['0 mph','5 mph','15 mph'];
-const sky = ['Sunny','Partly Cloudy','Cloudy'];
-const high = ['86','90','92'];
-const low = ['66','72','70'];
-const precip = ['2%','13%','10%'];
-
-// I thought it would be better to put variables inside an object
+// weather object 
 const weather = {
-    wind: ['0 mph','5 mph','15 mph'],
-    sky: ['Sunny','Partly Cloudy','Cloudy'],
     high: ['86','90','92'],
     low: ['66','72','70'],
-    precip: ['2%','13%','20%'],
+    sky: ['Sunny','Partly Cloudy','Cloudy'],
+    wind: ['0 mph','5 mph','15 mph'],
+    precipitation: ['2%','13%','10%'],
+    // activity: ["Today is a good day to swim", "Today is a good day to have a picnic", "Today is a good day to read a book"]
 };
-// Looping over the weather object to get 1 random match up for all key values
-for (const key in weather) {
-    //const randomWind = wind[Math.floor(Math.random() * wind.length)];
+
+//This function allows me to get random value 
+function randomSelectValue (items) {
+    const random = items[Math.floor(Math.random() * items.length)];
+    return random;
 };
+
+//This function allows me to get random value from weather object and assign it to a new object
+function forecast(options) {
+    return Object.keys(options).reduce((newWeather, key) => {
+        const value = options[key];
+        const selectedItem = randomSelectValue(value);
+        newWeather[key] = selectedItem;
+        return newWeather;
+    }, {});
+};
+
+// This logic allows me to assign the values in the day array as keys and the keys in the weather object as values in a new object
+const days = ['Friday', 'Saturday', 'Sunday'];
+const weekendWeather = days.reduce((initialValue, currentKey) => {
+    initialValue[currentKey] = forecast(weather);
+    return initialValue;
+}, {});
+
+// Bonus Activity based on weather
+// const activity = ["Swimming", "A picnic", "A BBQ"];
+// const activity = ["Today is a good day to swim", "Today is a good day to have a picnic", "Today is a good day to read a book"]
+// };
+
+// function getActivity(act) {
+//     return Object.keys(act).reduce((newActivity, Finalkey) => {
+//       const activity = act[Finalkey];
+//       const randomActivity = conditional statement ??
+      
+//       return newActivity;
+//   }, {});
+// };
+
+if (Object.values(weather.sky).includes('Sunny') > -1) {
+   console.log('Today is a good day to swim');
+}
+
+if (Object.values(weather.sky).includes('Partly Cloudy') > -1) {
+   console.log('Today is a good day to have a picnic');
+}
+
+if (Object.values(weather.sky).includes('Cloudy') > -1) {
+   console.log('Today is a good day to read a book');
+}
+
+//conditional statement
+if (weather === 'Sunny') {
+  console.log("It's sunny, today is a good day to go swimming");
+} else if (weather === 'Partly Cloudy') {
+  console.log("It's partly cloudy, today is a good day to have a picnic");
+} else if (weather === 'Cloudy') {
+  console.log("It's cloudy, today is a good day to stay in and read a book");
+} else {
+  console.log("I'm just going to chill today")
+}
+
+// const randomActivity = Object.values(weather).forEach(val => console.log(val));
+
+
+
+// const randomActivity = weather.reduce((activity, weather) => {
+//   activity[weather.sky] = weather.high;
+//   return activity;
+// }, {});
+
+// switch (weather.sky) {
+//   case "Sunny":
+//     console.log("Today is a good day to swim");
+//     break;
+//   case "Partly Cloudy":
+//     console.log("Today is a good day to have a picnic");
+//     break;
+//   case "Cloudy":
+//     console.log("Today is a good day to read a book");
+//     break;
+//   deafault:
+//     console.log("Today is a good day to do whatever you want");
+//     break;  
+// }
+
+// const activity = function(act) {
+//   if (act === "Sunny") {
+//     return "Today is a good day to swim";
+//   } else if (act === "Partly Cloudy") {
+//     return "Today is a good day to have a picnic";
+//   } else if (act === "Cloudy") {
+//     return "Today is a good day to read a book";
+//   } else {
+//     return "Today is a good day to do whatever you want"
+//   }
+// };
+
+
+
+// for (let prop in weather) {
+//   console.log(prop);
+// };
+
+// for (let prop in weather) {
+//   console.log(`${prop}: ${weather[prop]}`);
+// };
 
 
 const btn = document.querySelector('.btn');
@@ -35,6 +132,16 @@ btn.addEventListener('click', (e) => {
     e.preventDefault();
     console.log(e);
 });
+//This prints out and empty object to the console.
+document.getElementById("btn").onclick = function() {
+  document.getElementById("weather").innerHTML = weekendWeather;
+}
 
-// Bonus Activity based on weather
-const activity = ["Swimming", "A picnic", "A BBQ"];
+//Example using this 
+const button = document.getElementById('test');
+button.addEventListener('click' , function() {
+  console.log(this);
+  setTimeout(function() {
+    console.log(this);
+  }, 500)
+})
